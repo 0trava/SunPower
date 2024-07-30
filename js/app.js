3b72be103ff06a0460623f618087d09ac7b0757e
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2024-07-30 16:21:45 +0300

    add animate

--- js/app.js
+++ js/app.js
@@ -1,26 +1,4 @@
 
-
-
-    // Функція для відкриття форми
-    function openForm() {
-        document.getElementById("overlay").style.display = "block";
-    }
-
-    // Функція для закриття форми
-    function closeForm() {
-        document.getElementById("overlay").style.display = "none";
-    }
-
-
-
-    function openModal() {
-        document.getElementById('fullScreenModal').style.display = 'block';
-    }
-
-    function closeModal() {
-        document.getElementById('fullScreenModal').style.display = 'none';
-    }
-
     // Додаємо обробник подій до всіх посилань в модальному меню
     document.querySelectorAll('.modal-menu a').forEach(function(link) {
         link.addEventListener('click', function() {
@@ -46,6 +24,9 @@
 
         sections.forEach(section => observer.observe(section));
     });
+
+
+    // Animate with scroll
 
 
 
